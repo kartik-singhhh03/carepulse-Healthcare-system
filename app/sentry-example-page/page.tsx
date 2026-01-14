5cc6986e1b9f95b87d278afc46d346c4a0cb3446
Kartik Singh <kartiksingh3337@gmail.com>
2026-01-14 11:28:49 +0530

    changes in config

--- app/sentry-example-page/page.tsx
+++ app/sentry-example-page/page.tsx
@@ -68,10 +68,7 @@ export default function Page() {
 
         <p>
           Next, look for the error on the{" "}
-          <a href="https://javascript-mastery.sentry.io/issues/?project=4507458386526208">
-            Issues Page
-          </a>
-          .
+          <a href="https://sentry.io/issues/">Issues Page</a>.
         </p>
         <p style={{ marginTop: "24px" }}>
           For more information, see{" "}
